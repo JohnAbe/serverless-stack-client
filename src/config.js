01caf3c6a68c379f942717af23d1cac884bafcbe
John Abraham <johnabraham91@gmail.com>
2020-06-21 13:03:42 +0530

    Setting up our React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+s3: {
+    REGION:"us-east-1",
+    BUCKET:"notes-app-exercise"
+},
+apiGateway:{
+    REGION:"us-east-1",
+    URL:"https://1bnldgqzy3.execute-api.us-east-1.amazonaws.com/prod"
+},
+cognito:{
+    REGION:"us-east-1",
+    USER_POOL_ID:"us-east-1_l9kZX4nQk",
+    APP_CLIENT_ID:"2tteovuvh0inipe10asaro1pv0",
+    IDENTITY_POOL_ID:"us-east-1:8ebb32a5-5f5f-4178-82c0-85866902e731"
+}
+};
